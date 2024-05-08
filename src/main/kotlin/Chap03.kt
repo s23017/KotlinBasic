039e1025777ee7b098366b1c7d82ef6d1518107c
s23017 <s23017@std.it-college.ac.jp>
2024-05-08 15:17:35 +0900

    3,4,5章終わり

---
+++ src/main/kotlin/Chap03.kt
@@ -0,0 +1,10 @@
+package org.example
+
+fun main(args: Array<String>) {
+//    val message : String = "Hello World!"
+    val message = "こんにちは、世界!"
+    println(message)
+    println(message)
+    println(message)
+    println(message)
+}
---
+++ src/main/kotlin/Chap04.kt
@@ -0,0 +1,24 @@
+package org.example
+
+fun main(args: Array<String>) {
+    val a: Byte = 100
+    val b: Short = 10_000
+    val c: Int = 1_000_000_000
+    val d: Long = 1_000_000_000_000_000_000L
+    val e: Float = 0.123f
+    val f: Double = 0.123_456_789
+    val g: UByte = 150U
+    val h: UShort = 33_000U
+    val i: UInt = 2_200_000_000U
+    val j: ULong = 100_00000_0000_0000_0000UL
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+    println(i)
+    println(j)
+}
