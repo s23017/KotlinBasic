7ec52493472b0a8f633c4a4933b16e738132ffa0
s23017 <s23017@std.it-college.ac.jp>
2024-05-28 13:54:02 +0900

    Chap41コミット

--- src/main/kotlin/Chap41.kt
+++ src/main/kotlin/Chap41.kt
@@ -11,6 +11,15 @@ fun main(args: Array<String>) {
         box1.printValue()
         box2.printValue()
     }
+    run {
+        println("----41.7----")
+        val box1 = Box2<Int, String>()
+        val box2 = Box2<String, Double>()
+        box1.setValues(500, "ごひゃく")
+        box2.setValues("pi", 3.14)
+        box1.printValue()
+        box2.printValue()
+    }
 }
 
 class Box<T> {
@@ -24,5 +33,17 @@ class Box<T> {
 
     fun printValue() {
         println("$label : $content")
+    }
+}
+
+class Box2<L, C> {
+    var label: L? = null
+    var content: C? = null
+    fun setValues(label: L, content: C) {
+        this.label = label
+        this.content = content
+    }
+    fun printValue() {
+        println("${label} = ${content}")
     }
 }
